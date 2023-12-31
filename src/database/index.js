e7106c6950b4e7f7a7483c72c47fa5b494604b44
Alsttair <107138004+Alsttair@users.noreply.github.com>
2023-12-31 21:21:50 +0000

    Alterada a função conectar, para que esgote a quantidade de conexões do banco de dados

--- src/database/index.js
+++ src/database/index.js
@@ -1,17 +1,11 @@
 const { Pool } = require('pg');
 
-async function conectar() {
+const pool = new Pool({
+  connectionString: 'postgres://aluno_20201214010032:708280@177.136.201.182:5439/temp?schema=aluno_20191214010032'
+});
 
-  const pool = new Pool({
-    connectionString: 'postgres://aluno_20201214010032:708280@177.136.201.182:5439/temp?schema=aluno_20191214010032'
-  });
+//apenas testando a conexão. Tirei a função apenas para ele fazer o pool de conexoes apenas uma vez, porque quando eu fazia de muitas, dava erro
 
-  //apenas testando a conexão
-  const client = await pool.connect();
-  console.log("Criou pool de conexões no PostgreSQL!");
+console.log("Criou pool de conexões no PostgreSQL!");
 
-
-  return client;
-}
-
-module.exports = { conectar }
+module.exports = pool
